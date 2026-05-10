9086acec5de345d540fe5423bf172f4e6ce6f635
Vy <vongocthanhvy289@gmail.com>
2026-05-10 22:46:13 +0700

    REST integration and responsive UI

--- client/.next/dev/static/chunks/059b_next_link_0-kea~1.js
+++
@@ -1,8 +0,0 @@
-(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/client/node_modules/next/link.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/client/node_modules/next/dist/client/link.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=059b_next_link_0-kea~1.js.map
